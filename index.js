aff989d1a79e18455d3c83b2eb9ff8fb4f9c93c8
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-18 03:15:15 +0900

    사용할 미들웨어 적용

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
@@ -10,12 +14,11 @@ const handleHome = (req, res) => res.send("Hello from Home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenRoute = (req, res, next) => {
-  console.log("Between!");
-  next();
-};
-
-app.use(betweenRoute);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
