9f65b604b9915a17d2406ca413352669a34ec24e
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-18 01:23:33 +0900

    Express로 서버 생성

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hi!");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+function handleListening() {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
