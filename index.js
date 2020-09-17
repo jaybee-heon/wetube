aa8e627586cac25a261fea7968018feb754a14ae
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-18 01:43:33 +0900

    홈 화면과 프로필 화면의 get method handler를 추가하였음.

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function handleListening() {
   console.log(`Example app listening at http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from Home!");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
