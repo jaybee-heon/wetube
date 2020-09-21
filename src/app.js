deb88ccd6fff57d39adb4ecbbd6857e1bac91597
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-22 02:15:29 +0900

    Pug를 사용하여 View 적용

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -14,11 +15,17 @@ const handleHome = (req, res) => res.send("Hello from Home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+app.use(helmet());
+
+app.set("view engine", "pug");
+app.set("views", "src/views");
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
