e6fd4acbd55d39d4f487d587661a25fd55e2c39c
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-26 01:43:22 +0900

    edit video 페이지 구현

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,9 +11,9 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get(routes.home, home);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
