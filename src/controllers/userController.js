032fd0ac2d79c7a4cd45d12130d5fe490cb2e9ed
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-28 01:53:14 +0900

    회원가입 컨트롤러 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,25 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do : Register User
+    // To Do : Log User In
+    res.status(200);
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,14 +1,21 @@
 import express from "express";
 import routes from "../routes";
-import { join, login, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  login,
+  logout
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
