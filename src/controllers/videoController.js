d3bca169c83e8936f621bfaf62c07e3e49ae4d69
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-28 01:32:35 +0900

    홈 화면에서 비디오 리스트 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   const {
---
+++ src/db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 331231,
+    title: "Vidoe Awesome",
+    description: "This is Something I Love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1213,
+      name: "changheon",
+      email: "changheon@kaist.ac.kr"
+    }
+  },
+  {
+    id: 1415125,
+    title: "Vidoe TEST",
+    description: "Test",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1213,
+      name: "changheon",
+      email: "changheon@kaist.ac.kr"
+    }
+  },
+  {
+    id: 1345423,
+    title: "글쓰기를 습관화 하자",
+    description: "도파민 분비 체계? 보상 체계를 정상화하자",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1213,
+      name: "changheon",
+      email: "changheon@kaist.ac.kr"
+    }
+  }
+];
