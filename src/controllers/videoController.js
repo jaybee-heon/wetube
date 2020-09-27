29426b84dcd34b46b5609608affbd08422cedf12
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-09-28 01:37:43 +0900

    search 화면에서 결과  비디오리스트를 보일 수 있도록 html 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,7 +8,7 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const videoDetail = (req, res) =>
